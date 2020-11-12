eab0dd42680537ce01395b9e17faf6d1399da9f4
Hsin Ling Hu <hsinkava11@gmail.com>
2020-11-11 23:01:57 -0800

    create weather app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+const api={
+  key: "03a9bb44a700d09737ee77a8848c9558",
+  base: "http://api.openweathermap.org/data/2.5/weather"
+}
+export default class App extends React.Component {
 
-function App() {
+state={
+  temp:""
+}
+
+
+ getWeather=(e)=>{
+   e.preventDefault()
+   const {city, country}=e.target
+     fetch(`${api.base}?q=${city.value},${country.value}&units=metric&appid=${api.key}`)
+       .then(res => res.json())
+       .then(res => this.setState({
+         temp: res.main.temp
+       }))
+  }
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <form onSubmit={this.getWeather}>
+         <label htmlFor="city"></label>
+         <input type="text" name="city" placeholder="City"/>
+         <label htmlFor="country"></label>
+         <input type="text" name="country" placeholder="Country"/>
+         <button type="submit">Get Weather</button>
+       </form>
+      <p>weather:{this.state.temp}</p>
     </div>
   );
 }
+}
 
-export default App;
+
